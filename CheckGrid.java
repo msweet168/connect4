be967acbdd2319deafb89b786fca6def24752669
msweet168 <mitchellsweet168@aol.com>
2017-02-27 10:23:09 -0500

    Fixed bug where exception would be thrown when piece hit top row. Fixed bug where board would not detect a win diagonal down to the right.

--- CheckGrid.java
+++ CheckGrid.java
@@ -41,7 +41,7 @@ public class CheckGrid{
             }
             
             //check out of bounds
-            if((i-3)>=6||(j+3)<=7){   
+            if((i-3)>=0&&(j+3)<=6){   
          
             //check diagonal up to the right
                if(array[i][j]==check){
@@ -70,19 +70,19 @@ public class CheckGrid{
                }
             }
                
-           //  //check if it will go out of bounds
-//             if((i+3)<=5||(j+3)<=6){
-//             //check diagonal down to the right
-//                if(array[i][j]==check){
-//                   if(array[i+1][j+1]==check){
-//                      if(array[i+2][j+2]==check){
-//                         if(array[i+3][j+3]==check){
-//                            return true;
-//                         }
-//                      }
-//                   }
-//                }      
-//             }
+             //check if it will go out of bounds
+            if((i+3)<=5&&(j+3)<=6){
+            //check diagonal down to the right
+                if(array[i][j]==check){
+                   if(array[i+1][j+1]==check){
+                      if(array[i+2][j+2]==check){
+                         if(array[i+3][j+3]==check){
+                            return true;
+                         }
+                      }
+                   }
+                }      
+             }
          }
       }
       
